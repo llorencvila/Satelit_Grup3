8b6690d2ea91d2dc6959803377b18fffa6324b26
Llorenç <llorenc.vila@estudiantat.upc.edu>
2025-10-15 19:53:13 +0200

    parar i reprendre la transmisió i recepció de dades

--- Satèl·lit/10.10_P1DHT-PY/10.10_P1DHT-PY.ino
+++ Satèl·lit/10.10_P1DHT-PY/10.10_P1DHT-PY.ino
@@ -1,25 +1,29 @@
 #include "DHT.h"
 #include <Wire.h> 
 #include <SoftwareSerial.h>
+
 SoftwareSerial mySerial(10, 11); // RX, TX (azul, naranja)
-unsigned long nextMillis = 500;
+int interval = 1000;
+String data;
+unsigned long NextMillis;
 
 #define DHTTYPE DHT11   // DHT 11
 const int DHTPin = 5;   
 
 
 DHT dht(DHTPin, DHTTYPE);
+
 void setup(){
   Serial.begin(9600);
 
   mySerial.begin(9600);
   dht.begin();
 
-}
+  NextMillis = millis()+interval;
+  pinMode(8, OUTPUT);
 
-void loop() {
-
-  delay(1000);
+  }
+void DHTSendData(){
   float h = dht.readHumidity();
   float t = dht.readTemperature();
 
@@ -33,10 +37,34 @@ void loop() {
     mySerial.print(h);
     mySerial.print(":");
     mySerial.println(t);
+      }
+  return;
+  }
+
+void loop() {
+  if (mySerial.available()) {
+    data = mySerial.readString();
+    data.trim();
+    Serial.print(data);
+    }
+  if (data == "REANUDAR"){
+    digitalWrite(8,LOW);
+    Serial.println("Reanudem");
+    if (millis() >=NextMillis){
+      DHTSendData();
+      NextMillis = millis()+interval;
+      }
+    
+
+    } else if (data == "STOP"){
+      digitalWrite(8,HIGH);
+      Serial.println("Parant");
+    } else{
+      Serial.println("Sense_Dades");
+    }
   }
 
 
-}
 
 
 
