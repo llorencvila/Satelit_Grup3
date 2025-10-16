130bcaccd02c54f41a385ff21223f85dccb3be53
llorencvila <llorenc.vila@estudiantat.upc.edu>
2025-10-16 15:40:10 +0200

    Comentaris en el codi de Seriall.py

--- Estació de terra/PC/Seriall.py
+++ Estació de terra/PC/Seriall.py
@@ -7,16 +7,16 @@ mySerial = serial.Serial(device, 9600)
 print("funcionant:")
 
 t0= time.time()
-histH = []
-histT = []
-contact = []
-parametres = 2 #
+histH = [] #Llista on es guarden totes les deades de humitat a mode de historial
+histT = [] #Llista on es guarden totes les temperatures a mode de historial
+contact = [] #Llista on es guarden tots els temps (des de l'inici del programa) en que s'han rebut dades des de la estació de terra. (Serveix com a historial i es pot utilitzar per trobar errors de comunicació)
+parametres = 2 #Numero de paràmetres separats per ":" que el programa espera rebre. Si rep menys que dita quanitat donarà com a invàlida la recepció de dades per tal d'evitar problemes més endevant
 plt.ion()
 
-def temps():
-   return time.time()-t0
+def temps(): #retorna el temps transcorregut des de l'inici del programa 
+   return time.time()-t0 
    
-def GrafiquesSeparades():
+def GrafiquesSeparades(): #Funció per crear una sol element amb dues gràfiques (Temperatura i humitat)
    plt.subplot(2,1,1)
    plt.title("Temp (º) / t (s)")
    plt.plot(contact, histT)
@@ -26,9 +26,9 @@ def GrafiquesSeparades():
 
    plt.show()
    return
-def GTemp():
+def GTemp(): #Funció per crear una sola gràfica de temperatura
    return
-def GHum():
+def GHum(): #Funció per crear una sola gràfica d'hunmitat
    return
 def GDobleEix():#Work in progress
    return
