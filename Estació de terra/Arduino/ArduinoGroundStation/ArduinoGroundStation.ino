1f1bcf14b535de8868bbf37707d3714718c47de4
Llorenç <llorenc.vila@estudiantat.upc.edu>
2025-10-17 13:16:04 +0200

    Update ArduinoGroundStation.ino

--- Estació de terra/Arduino/ArduinoGroundStation/ArduinoGroundStation.ino
+++ Estació de terra/Arduino/ArduinoGroundStation/ArduinoGroundStation.ino
@@ -1,27 +1,31 @@
 #include <SoftwareSerial.h>
 SoftwareSerial mySerial(10, 11);  // RX, TX (azul, naranja)
-unsigned long nextMillis = 500;
+
 
 void setup() {
   Serial.begin(9600);
   mySerial.begin(9600);
+  pinMode(13, OUTPUT);
 }
 void loop() {
   
-  if (mySerial.available()) {
-    String data = mySerial.readString();
-    Serial.print(data);
+  if (mySerial.available()) { //llegeix les dades del satel·lit i les fica a l'ordinador
+    String dataSat = mySerial.readString();
+    Serial.print(dataSat);
+      if (dataSat == "FALLO"){
+        digitalWrite(13, HIGH);
+      }
   } 
 
-  if (Serial.available()) {
+  if (Serial.available()) { //agafa les instruccions del pc i les envia al satel·lit
     String dataPc = Serial.readString();
     if (dataPc == "STOP") {
       mySerial.println("STOP");
       digitalWrite(5,HIGH);
-
     } else if (dataPc == "REANUDAR") {
       mySerial.println("REANUDAR");
       digitalWrite(5,LOW);
     }
   }
+  
 }
